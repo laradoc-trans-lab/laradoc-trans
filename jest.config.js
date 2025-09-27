17e3884c0b64646f7af4411ba09d0a81d63c70cc
pigochu <pigochu@gmail.com>
2025-09-27 19:11:10 +0800

    使用 babel 轉譯讓測試能通過

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,8 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.ts'],
+  transformIgnorePatterns: [],
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest'
+  },
 };
